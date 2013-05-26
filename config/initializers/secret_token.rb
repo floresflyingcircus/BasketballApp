ef8f45a4139069f9ccc7926448934c5fa0ed18dd
Darlene Flores <floresflyingcircus@yahoo.com>
2013-05-26 17:28:50 -0400

    first commit to BasketballApp

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BasketballApp::Application.config.secret_token = 'b6d1fee37640b2576870194496995601a2d9897358d6c5140b718ca13097ea188f0ee9e3398a92d563ebf456e558baddb81a06b7a9482255d40df655c49c5cd0'
