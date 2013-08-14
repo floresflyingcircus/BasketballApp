d885d278e0fa508a552e5a2baa07e259a0a75b3c
Darlene Flores <floresflyingcircus@yahoo.com>
2013-08-13 22:42:43 -0400

    removed <<< etc from player.rb.BACKUP.649.rb

--- app/models/player.rb.BACKUP.649.rb
+++ app/models/player.rb.BACKUP.649.rb
@@ -1,8 +1,8 @@
 class Player < ActiveRecord::Base
   has_many :stats
-<<<<<<< HEAD
-  attr_accessible :in_game, :name, :number
-=======
 
->>>>>>> 0c3695b77a0a0902776afac9d57b3a20b06a235d
+  attr_accessible :in_game, :name, :number
+
+
+
 end
